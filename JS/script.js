3d04f12ce7373c629acd0a26913566ea58e0ccf7
RAIMONES BARROS <raimonesrg3@gmail.com>
2022-12-02 18:55:58 -0300

    asd

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,30 @@
 const displayUp = document.querySelector("#up");
 const displayDown = document.querySelector("#down");
 const number = document.querySelectorAll(".number");
+const operation = document.querySelectorAll(".operation");
+const delet = document.querySelector(".del");
+const clear = document.querySelector(".c");
+const clearAll = document.querySelector(".clear");
+const equal = document.querySelector(".equal");
 
+class Calculator {
+  constructor(displayUp, displayDown) {
+    this.displayUp = displayUp;
+    this.displayDown = displayDown;
+  }
 
-function number() {
-  estou com bloqueio
+  clear(){
+    this.displayUp = '';
+    this.displayDown = '';
+    this.operation = undefined;
+  }
+
+  updateDisplay() {
+    this.displayUp.innerText = this.displayUp;
+    this.displayDown.innerText = this.displayDown;
+  }
 }
 
-number.addEventListner("click", number)
+const calculator = new Calculator(
+  displayUp, displayDown
+);
