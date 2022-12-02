a56ff25fc58b214ea21c647c9afca85edf54eff9
RAIMONES BARROS <raimonesrg3@gmail.com>
2022-12-02 15:59:16 -0300

    add class key and function script

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+const displayUp = document.querySelector("#up");
+const displayDown = document.querySelector("#down");
+const number = document.querySelectorAll(".number");
+
+
+function number() {
+  
+}
+
+number.addEventListner("click", number)
