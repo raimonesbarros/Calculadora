37b12df7a9fe13ba6b460522ced874d9fe8fa2b2
RAIMONES <raimonesrg3@gmail.com>
2023-01-25 17:42:35 -0300

    js principal

--- JS/main.js
+++ JS/main.js
@@ -33,7 +33,41 @@ operator.forEach(o=>{
 })
 
 // Recebe as informações das funções extras
-del  .addEventListener('click', ()=>calculator.delete())
-c    .addEventListener('click', ()=>calculator.clear())
-clear.addEventListener('click', ()=>calculator.clearAll())
-equal.addEventListener('click', ()=>calculator.equal())
+del  .addEventListener('click',()=>calculator.delete  ())
+c    .addEventListener('click',()=>calculator.clear   ())
+clear.addEventListener('click',()=>calculator.clearAll())
+equal.addEventListener('click',()=>calculator.equal   ())
+
+// Recebe entradas do teclado
+document.addEventListener('keyup', evt=>{
+
+  // Entradas numéricas
+  if(evt.key >=0){
+    calculator.addNumber(evt.key)
+  }
+  // Outras entradas
+  switch (evt.key) {
+    case        '.' : calculator.addNumber(evt.key)
+      break;
+    case        ',' : calculator.addNumber('.')
+      break;
+    case        '+' : calculator.addOperator(evt.key)
+    break;
+    case        '-' : calculator.addOperator(evt.key)
+      break;
+    case        '*' : calculator.addOperator('x')
+      break;
+    case        '/' : calculator.addOperator('÷')
+      break;
+    case    'Enter' : calculator.equal()
+      break;
+    case 'Backspace': calculator.delete()
+      break;
+    case   'Delete' : calculator.clear()
+      break;
+    case   'Escape' : calculator.clearAll()
+      break;
+    default         : ''
+      break;
+  }
+})
