947ba1b12c13df34eb52d927bded893fa7aa7d74
RAIMONES <raimonesrg3@gmail.com>
2023-01-25 17:42:48 -0300

    class calculator

--- JS/Calculator.js
+++ JS/Calculator.js
@@ -11,31 +11,49 @@ export class Calculator{
 
   // Adciona o valor do número recebido
   addNumber(num){
-    this._current += num
+      // Controla o uso único do ponto 'vírgula'
+    if(num == '.' && this._current.includes('.')){
+      return
+    } else if(this._current.length >= 14){
+      // Impede o uso de mais de 14 caracteres
+      this._current = this._current.slice(0, 14)
+    } else if(num == '.' && !this._current){
+      this._current = 0 + num
+    } else{
+      this._current += num
+    }
     // Pede atualização do display
-    this.updateDisplay(this._current)
+    if(this._current.includes('.')){
+      this.updateDisplay(this._current.replace('.', ','))  
+    } else {
+      
+      this.updateDisplay(this._current)
+    }
   }
 
   // Manipula o operador recebido
   addOperator(op){
+
     let last = this._operation.slice(-1)
-    // Controla o uso do ponto no fim do valor
+    // Controla o uso do ponto no fim do valor, retirando-o caso não tenha casas decimais
     if(this._current.slice(-1) == '.'){
       this._current = this._current.slice(0, -1)
     }
-    // Controla a alteração de operador
+    // Controla a mudança de operador sem prejuíjo para a operação
     if(!this._current && last == '+'
     || !this._current && last == '-'
     || !this._current && last == 'x'
     || !this._current && last == '÷'){
       this._operation = this._operation.slice(0,-1)
-    // Impede o uso do operador
+    // Impede o uso do operador quando não pode ser usado.
     } else if(!this._result && !this._current){
       return
-    // Recebe o primeiro operador
+    // Recebe o primeiro operador para começar as operações
     } else if(!this._result && this._current){
       this._result = this._current
     // Libera a operação dos valores
+    } else if(this._result && !this._current){
+      this._current = this._result
     } else if(this._result  && this._current){
       this.toOperate()
     }
@@ -51,15 +69,19 @@ export class Calculator{
   toOperate(){
     switch (this._operator) {
       case '+':
+        // Operação de soma
         this._result = Number(this._result) + Number(this._current)
         break;
       case '-':
+        // Operação de subtração
         this._result -= this._current
         break;
       case 'x':
+        // Operação de multiplicação
         this._result *= this._current
         break;
       case '÷':
+        // Operação de divisão ou quociente
         this._result /= this._current
         break;
       default: ''
@@ -69,7 +91,7 @@ export class Calculator{
 
   // Retorna o resultado
   equal(){
-    // Controla o uso do ponto no fim do valor
+    // Controla o uso do ponto no fim do valor, retirando-o caso não tenha casas decimais
     if(this._current.slice(-1) == '.'){
       this._current = this._current.slice(0, -1)
     }
@@ -84,14 +106,14 @@ export class Calculator{
     // Pede atualização do display
     this.updateDisplay(this._result)
     this._current    = ''
-    this._operator   = ''
-    this._result     = ''
-    this._operation  = ''
+    //this._operator   = ''
+    //this._result     = ''
+    this._operation  = this._result + this._operator
   }
 
   // Apaga o ultimo número digitado
   delete(){
-    if(this._current > 0){
+    if(this._current.length > 0){
       this._current  = this._current.slice(0, -1)
       // Pede atualização do display
       this.updateDisplay(this._current)
@@ -102,7 +124,7 @@ export class Calculator{
   clear(){
     this._current    = ''
     // Pede atualização do display
-    this.updateDisplay(this._current)
+    this.updateDisplay(0)
   }
 
   // Limpa todas as informações e reinicia
@@ -113,12 +135,15 @@ export class Calculator{
     this._result     = ''
     
     // Pede atualização do display
-    this.updateDisplay(this._current)
+    this.updateDisplay(0)
   }
 
   // Atualiza o display
   updateDisplay(dDown){
-    this._dDown.innerHTML = dDown
+ 
+    // Mostra o resultado no padrão brasileiro ex: 9821.23 => 9.821,23   
+    this._dDown.innerHTML = dDown.toLocaleString('pt-br', {style: 'decimal', maximumFractionDigits: 2})
     this._dUp.innerHTML   = this._operation
+
   }
 }
