8c3d54c1cdadfbc98c4d4780ad7da61369ce5715
RAIMONES <raimonesrg3@gmail.com>
2023-02-04 14:50:36 -0300

    Create Controller.js

---
+++ js/poo/controller/Controller.js
@@ -0,0 +1,15 @@
+import { Calculator } from "../model/Calculator.js"
+
+export class Controller{
+
+  constructor(name, content){
+    this.name    = name
+    this.content = content
+
+    this.receptor = []
+  }
+  
+
+
+
+}
