7f968fff8c8db61126f64e4441fb9d4d59f6c3c5
RAIMONES <raimonesrg3@gmail.com>
2023-02-06 13:17:46 -0300

    bug

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import { Calculator } from './poo/model/Calculator.js'
-import { View } from './poo/view/View.js'
+import { Calculator } from './js/poo/model/Calculator.js'
+import { View } from './js/poo/view/View.js'
 
 let calculator = new Calculator()
 
